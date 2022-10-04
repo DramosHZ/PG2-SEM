c88ba11b6d15cc013781f4f1cb9dbbcfb28273ef
Dennis Andres Ramos Hernández <ar328208@gmail.com>
2022-10-04 11:42:36 -0600

    Update 4/10/2022

---
+++ PG2/Controllers/AtencionRequerimientosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PG2.Controllers
+{
+    public class AtencionRequerimientosController : Controller
+    {
+        // GET: AtencionRequerimientos
+        public ActionResult AtencionReq()
+        {
+
+            var test = new testContext();
+
+            return View(test.);
+        }
+
+    }
+}
---
+++ PG2/Controllers/RequerimientosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PG2.Controllers
+{
+    public class RequerimientosController : Controller
+    {
+        // GET: Requerimientos
+        public ActionResult Requerimiento()
+        {
+            return View();
+        }
+
+
+
+    }
+}
